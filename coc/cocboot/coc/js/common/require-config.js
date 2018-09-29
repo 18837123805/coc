3470b83589a662f53e7788d23111a3c53b14a9c5
18837123805 <m18837123805@163.com>
2018-09-29 15:02:36 +0800

    ISCROLL.JS引用

--- coc/cocboot/coc/js/common/require-config.js
+++ coc/cocboot/coc/js/common/require-config.js
@@ -3,7 +3,9 @@ require.config({
 	paths:{
 		"jquery":"jquery",
 		"bootstrap":"bootstrap",
-		"swiper":"swiper"
+		"swiper":"swiper",
+		"iscroll":"iscroll",
+		"demoUtils":"demoUtils"
 	},
 	map:{
 		'*':{
@@ -19,24 +21,40 @@ require.config({
 	    },
 	    swiper:{
 	      	deps:['jquery']
+	    },
+	    iscroll:{
+	    	deps:['jquery']
 	    }
 	}
 });
 
-require(["jquery","swiper"],function($,Swiper){
-	var swiper = new Swiper('.swiper-container', {
-		width: window.innerWidth,
-		loop: true,
-		autoplay: {
-    		delay: 2000,
-		    stopOnLastSlide: false,
-		    disableOnInteraction: true,
-	    },
-	    pagination: {
-	    	el: '.swiper-pagination',
-	    	type: 'bullets',
-	    }
-    });
+require(["jquery","swiper","iscroll","demoUtils"],function($,Swiper,IScroll){
+	window.onload = function() {
+		var swiper = new Swiper('.swiper-container', {
+			width: window.innerWidth,
+			loop: true,
+			autoplay: {
+	    		delay: 2000,
+			    stopOnLastSlide: false,
+			    disableOnInteraction: true,
+		    },
+		    pagination: {
+		    	el: '.swiper-pagination',
+		    	type: 'bullets',
+		    }
+	    });
+	    var myScroll = new IScroll('#wrapper', {
+	    	mouseWheel: true,
+	    	scrollbars: true,
+			scrollX: true,
+			scrollY: true,
+			momentum: true,
+			snap: true
+			// bounceEasing: 'elastic',
+			// bounceTime: 1200,
+			// freeScroll: true
+		});
+	}
 })
 
 
--- coc/cocboot/coc/js/page/index.js
+++ coc/cocboot/coc/js/page/index.js
@@ -1,16 +1,9 @@
 require(["jquery","bootstrap","css!../../css/page/index.css"],function($,bootstrap){
 	$(function(){
-		
-		// $('.navbar').on('activate.bs.scrollspy', function () {
-		// })
 		$('body').scrollspy({ target: '.navbar' })	
 		$('[data-spy="scroll"]').each(function () {
 		  	var $spy = $(this).scrollspy('refresh');
 		});
-		// var searchfun = function(){
-		// 	$('#inputBox').addClass('myKeyframes')
-		// }
-		// $('.icon-search').bind('click',searchfun);
 	})
 }
 )
